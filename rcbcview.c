ac237a93c797bdba2bf3c5dd02e1c2e1d3bce4a3
David C. Bishop <DavidCosburnBishop@gmail.com>
2009-03-12 02:50:15 +1100

    Added key to switch between wireframe mode, quit.

--- rcbcview.c
+++ rcbcview.c
@@ -21,6 +21,12 @@
 #define COLOUR_CUBE COLOUR_WHITE_3F
 #define BOX_SIZE 1.0f
 
+#define KEY_ESC 27
+#define KEY_q 113
+#define KEY_Q 81
+#define KEY_w 119
+#define KEY_W 87
+
 struct globals {
 	unsigned int height;
 	unsigned int width;
@@ -39,29 +45,31 @@ struct globals {
 	float cam_rot_x_temp;
 	float cam_rot_y_temp;
 	float cam_fov;
+
+	int wireframe;
 };
 
 struct globals g;
 
 /* Position camera around box using some voodoo math */
 void cameraPosition() {
-   /* http://en.wikipedia.org/wiki/Spherical_coordinates */
-   float theta = g.cam_rot_x+g.cam_rot_x_temp;
-   float phi = g.cam_rot_y+g.cam_rot_y_temp;
-   
-   g.cam_x = g.cam_zoom * (cos(theta*(PI/180))) * (sin(phi*(PI/180)));
-   g.cam_y = g.cam_zoom * (cos(phi*(PI/180)));
-   g.cam_z = g.cam_zoom * (sin(theta*(PI/180))) * (sin(phi*(PI/180)));
-   
-   gluLookAt( g.cam_x, g.cam_y, g.cam_z, 0.0f, 0.0f, 0.0f, 0.0f, 10.0f, 0.0f );
+	/* http://en.wikipedia.org/wiki/Spherical_coordinates */
+	float theta = g.cam_rot_x+g.cam_rot_x_temp;
+	float phi = g.cam_rot_y+g.cam_rot_y_temp;
+	
+	g.cam_x = g.cam_zoom * (cos(theta*(PI/180))) * (sin(phi*(PI/180)));
+	g.cam_y = g.cam_zoom * (cos(phi*(PI/180)));
+	g.cam_z = g.cam_zoom * (sin(theta*(PI/180))) * (sin(phi*(PI/180)));
+	
+	gluLookAt( g.cam_x, g.cam_y, g.cam_z, 0.0f, 0.0f, 0.0f, 0.0f, 10.0f, 0.0f );
 }
 
 /* Set 3D perspective mode */
 void setPerspective() {
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   gluPerspective(g.cam_fov, (GLfloat)g.width / (GLfloat)g.height, 0.1f, 10000.0f);
-   glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(g.cam_fov, (GLfloat)g.width / (GLfloat)g.height, 0.1f, 10000.0f);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void displayFunc() {
@@ -106,66 +114,91 @@ void displayFunc() {
 		errorit("GLUT: %s", gluErrorString(error));
 }
 
-void reshapeFunc(int width, int height) {
-   if (height==0) {
-      height=1;
-   }
-   g.height = height;
-   g.width = width;
-   glViewport(0, 0, width, height);
+void setPolygonMode() {
+	if(g.wireframe) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	} else {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+}
 
-   setPerspective();
+void reshapeFunc(int width, int height) {
+	if (height==0) {
+		height=1;
+	}
+	g.height = height;
+	g.width = width;
+	glViewport(0, 0, width, height);
+
+
+	setPerspective();
 }
 
 /* Handle mouse roation */
 void motionFunc(int x, int y) {
-   int x_diff, y_diff;
+	int x_diff, y_diff;
 
-   x_diff = x - g.mouse_x;
-   y_diff = y - g.mouse_y;
+	x_diff = x - g.mouse_x;
+	y_diff = y - g.mouse_y;
 
-   g.cam_rot_x_temp = ((float)x_diff / g.width) * 100;
-   g.cam_rot_y_temp = -((float)y_diff / g.height) * 100;
-   
-   /* Stop flipping over the top of the cube */
-   if(g.cam_rot_y_temp + g.cam_rot_y < Y_LOCK) {
-      g.cam_rot_y_temp = Y_LOCK-g.cam_rot_y;
-   } else if(g.cam_rot_y_temp + g.cam_rot_y > 180.0f - Y_LOCK) {
-      g.cam_rot_y_temp = (180.0f-Y_LOCK)-g.cam_rot_y;
-   }
+	g.cam_rot_x_temp = ((float)x_diff / g.width) * 100;
+	g.cam_rot_y_temp = -((float)y_diff / g.height) * 100;
+	
+	/* Stop flipping over the top of the cube */
+	if(g.cam_rot_y_temp + g.cam_rot_y < Y_LOCK) {
+		g.cam_rot_y_temp = Y_LOCK-g.cam_rot_y;
+	} else if(g.cam_rot_y_temp + g.cam_rot_y > 180.0f - Y_LOCK) {
+		g.cam_rot_y_temp = (180.0f-Y_LOCK)-g.cam_rot_y;
+	}
 }
 
 /* When mouse clicked */
 void mouseFunc(int button, int state, int x, int y) {
-   switch(button) {
-      case(0):
-         if(!state) {   /* Save the current x,y cords when starting to drag */
-            g.mouse_x = x;
-            g.mouse_y = y;
-         } else { /* Store the temp rotation as permenate when finished */
-            g.cam_rot_x+=g.cam_rot_x_temp;
-            g.cam_rot_y+=g.cam_rot_y_temp;
-            g.cam_rot_x_temp = 0.0f;
-            g.cam_rot_y_temp = 0.0f;
-         }
-         break;
-      case(4): /* Zoom out with mouse wheel */
-         g.cam_zoom+=ZOOM_STEP;
-         if(g.cam_zoom>ZOOM_MAX)
-            g.cam_zoom=ZOOM_MAX;
-         logit("Zoom at %f.", g.cam_zoom);
-         break;
-      case(3): /* Zoom in with mouse wheel */
-         g.cam_zoom-=ZOOM_STEP;
-         if(g.cam_zoom<ZOOM_MIN)
-            g.cam_zoom=ZOOM_MIN;
-         logit("Zoom at %f.", g.cam_zoom);
-         break;
-   }
+	switch(button) {
+		case(0):
+			if(!state) {	/* Save the current x,y cords when starting to drag */
+				g.mouse_x = x;
+				g.mouse_y = y;
+			} else { /* Store the temp rotation as permenate when finished */
+				g.cam_rot_x+=g.cam_rot_x_temp;
+				g.cam_rot_y+=g.cam_rot_y_temp;
+				g.cam_rot_x_temp = 0.0f;
+				g.cam_rot_y_temp = 0.0f;
+			}
+			break;
+		case(4): /* Zoom out with mouse wheel */
+			g.cam_zoom+=ZOOM_STEP;
+			if(g.cam_zoom>ZOOM_MAX)
+				g.cam_zoom=ZOOM_MAX;
+			logit("Zoom at %f.", g.cam_zoom);
+			break;
+		case(3): /* Zoom in with mouse wheel */
+			g.cam_zoom-=ZOOM_STEP;
+			if(g.cam_zoom<ZOOM_MIN)
+				g.cam_zoom=ZOOM_MIN;
+			logit("Zoom at %f.", g.cam_zoom);
+			break;
+	}
 }
 
 void idleFunc() {
-   glutPostRedisplay();
+	glutPostRedisplay();
+}
+
+/* Handle key presses */
+void keyboardFunc(unsigned char key, int x, int y) {
+	switch(key) {
+		case(KEY_ESC): case(KEY_q): case(KEY_Q): /* ESC / Q */
+			exit(1);
+			break;
+		case(KEY_w): case(KEY_W): /* Toggle wireframe on/off */
+			g.wireframe = !g.wireframe;
+			setPolygonMode();
+			break;
+		default:
+			debugit(DEBUG_LOW, "Pressed unknown key: %d\n", key);
+			break;
+	}
 }
 
 int main(int argc, char** argv) {
@@ -184,7 +217,7 @@ int main(int argc, char** argv) {
 	g.height = 600;
 	g.width = 800;
 
-	logit("Initilizing RCRB Viewer GLUT...");
+	logit("Initilizing RCRB GLUT Viewer...");
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
@@ -197,6 +230,7 @@ int main(int argc, char** argv) {
 	glutReshapeFunc(&reshapeFunc);
 	glutMouseFunc(&mouseFunc);
 	glutMotionFunc(&motionFunc);
+  glutKeyboardFunc(&keyboardFunc);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClearDepth(10000.0f);
@@ -207,6 +241,9 @@ int main(int argc, char** argv) {
 
 	g.mouse_x = 0;
 	g.mouse_y = 0;
+
+	g.wireframe = 1;
+	setPolygonMode();
 
 	g.cam_zoom = 2.5f;
 	g.cam_rot_x = 25.0f;
